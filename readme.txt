f0faef4fa3439870ced3e1b2a1787088b6fe4591
sahil <aqsagaus@gmail.com>
2025-11-23 11:31:45 +0530

    first commit

---
+++ readme.txt
@@ -0,0 +1,16 @@
+npm init -y
+
+npm i express
+
+npm i nodemailer
+
+npm i mongoose
+
+npm i ejs
+
+git init
+git status
+git config --global users.name "sahil"
+git config --global users.email "sahil@gmail.in"
+
+git remote add origin https://github.com/sahilppawar17-bit/NodeJS-login-and-Register-.git
---
+++ src/app.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cdac2025")
+  .then(() => console.log("Connected!"));
+
+const Schema = mongoose.Schema;
+const BlogPost = new Schema({
+  name: String,
+  email: String,
+  password: Number,
+});
+
+const MyModel = mongoose.model("users", BlogPost);
+const express = require("express");
+const app = express();
+//to accept data in form type and not in json format
+app.use(express.urlencoded);
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.get("/users", async (req, res) => {
+  try {
+    const result = await MyModel.find();
+    res.render("users.ejs", { data: result });
+  } catch (err) {
+    res.send("ERROR : " + err);
+  }
+  //  res.render("users.ejs");
+});
+app.post("/registerAction", async (req, res) => {
+  //Here data will be submitted
+  try {
+    let record = new MyModel(req.body);
+    await record.save();
+    res.redirect("/users");
+  } catch (err) {
+    res.send("ERROR : " + err);
+  }
+});
+app.listen(3000);
